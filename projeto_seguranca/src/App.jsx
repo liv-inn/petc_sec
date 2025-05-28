6a6e4dfd1759c04c17e0da20bf06391b02ec9293
liv-inn <liviaavelino2002@gmail.com>
2025-05-28 20:49:07 -0300

    landing page

--- projeto_seguranca/src/App.jsx
+++ projeto_seguranca/src/App.jsx
@@ -1,7 +1,17 @@
-
-
+import LandingPage from '../src/pages/LandingPage.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+
+  return(
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/landing_page" element={<LandingPage/>} />
+      </Routes>
+    </BrowserRouter>
+    
+  )
   
 }
 
---
+++ projeto_seguranca/src/components/Logo.jsx
@@ -0,0 +1,8 @@
+function Logo() {
+
+    return(
+        <h1 className="text-5xl font-bold text-gray-800">🐾 PetCare </h1>
+    )
+}
+
+export default Logo;
